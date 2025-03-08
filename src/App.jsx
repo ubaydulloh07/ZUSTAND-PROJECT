a4a73fab870241cdc683b09df5c4a363dcad11db
ubaydulloh07 <ubaydullohmirzaaxmedov@gmail.com>
2025-03-09 01:09:46 +0500

    login-add

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,5 @@
 
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Header from "./components/header/header";
-// import Home from "./pages/Home/home";
-// import Liked from "./pages/Liked/liked";
-// import Cart from "./pages/Cart/cart";
-// import Add from "./pages/add-page/add";
-// import Login from "./pages/login/login";
-// import Profile from "./pages/profile/profile";
-// import Products from "./pages/product-page/product";
 
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Header />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/liked" element={<Liked />} />
-//         <Route path="/cart" element={<Cart />} />
-//         <Route path="/add" element={<Add />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/profile" element={<Profile />} />
-//         <Route path="/products" element={<Products />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
 
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -35,7 +8,7 @@ import Home from "./pages/Home/home";
 import Liked from "./pages/Liked/liked";
 import Cart from "./pages/Cart/cart";
 import Add from "./pages/add-page/add";
-import Profile from "./pages/Profile/profile";
+import Profile from "./pages/profile/profile";
 import Login from "./pages/Login/login";
 import useAuth from "./store/useAuth";
 import Products from "./pages/product-page/product";
